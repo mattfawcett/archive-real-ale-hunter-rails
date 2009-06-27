e12eabdf7398afafad30e2c30ba852116fd1b2fa
Matt Fawcett <mail@matthewfawcett.co.uk>
2009-06-27 23:09:37 +0100

    added jquery, got JS "add extra beer" thing working

--- app/helpers/pubs_helper.rb
+++ app/helpers/pubs_helper.rb
@@ -1,2 +1,11 @@
 module PubsHelper
+  def add_beer_link(name, form)
+    link_to_function name do |page|
+      beer = render(:partial => 'beer', :locals => { :beer_form => form, :beer => Beer.new })
+      page << %{
+var new_beer_id = "new_" + new Date().getTime();
+$('#beers_area').append("#{ escape_javascript beer }".replace(/new_\\d+/g, new_beer_id) );
+}
+    end
+  end
 end
