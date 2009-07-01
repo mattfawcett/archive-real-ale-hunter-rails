a3ed0691e17a26dd209c6056ee396389dbbbd753
mattfawcett <matt@bolseragency.com>
2009-07-01 08:58:17 +0100

    started a view spec to make sure that the name field doesn't show up on edit. Needs finishing

---
+++ spec/views/pubs/edit.haml_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "/pubs/edit.haml" do
+  include PubsHelper
+  
+  before(:each) do
+    assigns[:pub] = @pub = Pub.make
+  end
+
+  it "should render edit form" do
+    render "/pubs/edit.haml"
+    
+    response.should have_tag("form[action=#{pub_path(@pub)}][method=post]") do
+      without_tag('input#pub_name[name=?]', "pub[name]")
+      with_tag('input#description[name=?]', "pub[description]")
+    end
+  end
+end
