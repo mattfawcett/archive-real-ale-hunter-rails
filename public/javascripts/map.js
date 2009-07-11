34691c55a8621d02d76968fc28f9f9857ed3467a
Matt Fawcett <mail@matthewfawcett.co.uk>
2009-07-11 21:35:16 +0100

    formatting improvement. Need to fix memory leak

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 
 
 var icon = new GIcon(G_DEFAULT_ICON);  
-var markers = []; 
+var markers = [];
+var non_clustered_markers = [];
 var markerCluster;
 var clustering_on = true;
 var map;
@@ -15,9 +16,6 @@ function initialize() {
     map = new GMap2(document.getElementById('map'));
     map.setCenter(new GLatLng(54, -1), 6);
     map.addControl(new GLargeMapControl());
-    
-    
-    
     
     $.getJSON('/pubs.json','', function(json)
    	{
@@ -31,25 +29,30 @@ function initialize() {
       markerCluster = new MarkerClusterer(map, markers, {gridSize: 50, maxZoom: 14});
     });
     
+    GEvent.addListener(map, "zoomend", function(overlay, point)
+ 	  {
+      remove_non_clustered_markers();
+    });
+    
     GEvent.addListener(map, "moveend", function(overlay, point)
-	  {
+	  {			
 			var curzoom = map.getZoom();
 			if(curzoom >= 14)
 			{
 				//we are zoomed in, dynamically update numbered markers			
 				markerCluster.clearMarkers();
-        clustering_on = false;
+        clustering_on = false; //set flag si if we zoom out again, clustering gets restarted
         update_non_clustered_markers();
 			}
 			else
 			{
 				//we are zoomed out, just use the clustering
 				if(!clustering_on){
+					clustering_on = true; //set flag again so we don't keep clustering
   				markerCluster = new MarkerClusterer(map, markers, {gridSize: 50, maxZoom: 14});
 				}
 			}
-		});				
-    
+		});				    
   }
 }
 
@@ -57,43 +60,42 @@ function initialize() {
 //create a marker object for the cluster, (if a number is passed in then a numbered icon will be used)
 function createMarker(point, pub_id, number)
 {
-		if(number)
-		{
-			var icon = numbered_icon(number);
-			var marker = new GMarker(point, icon);			
-		}
-		else
-		{
-		  var marker = new GMarker(point, icon);
-		}
-		
-	  GEvent.addListener(marker, "click", function() {marker.openInfoWindowHtml(marker_info_window_html_for_pub(pub_id));});
-		return marker;
-	};
-	
-	
-	//for a particular pub_id, generate the HTMl for its overlay
-	function marker_info_window_html_for_pub(pub_id)
+  if(number)
 	{
-		var html = '';
-		$.ajax({
-   type: 'GET',
-  url: '/pubs/' + pub_id + '.json',
-  dataType: 'json',
-  success: function(json) {
-    html = html + '<strong><a target="_blank" href="/pubs/' + json.pub.slug.slug.name + '">' + json.pub.name + '</a></strong><br/>';              
-    html = html + json.pub.address_1 + '<br/>';
-    if(json.pub.address_2){
-      html = html + json.pub.address_2 + '<br/>';
-    }
-    html = html + json.pub.town + '<br/>';
-    html = html + json.pub.post_code + '<br/>';
-    html = html + json.pub.telephone + '<br/>';
-  },
-  data: {},
-  async: false
-});
-return html;
+  	var icon = numbered_icon(number);
+		var marker = new GMarker(point, icon);			
+	}
+	else
+	{
+    var marker = new GMarker(point, icon);
+	}
+		
+	GEvent.addListener(marker, "click", function() {marker.openInfoWindowHtml(marker_info_window_html_for_pub(pub_id));});
+	return marker;
+};
+	
+//for a particular pub_id, generate the HTMl for its overlay
+function marker_info_window_html_for_pub(pub_id)
+{
+	var html = '';
+	$.ajax({
+    type: 'GET',
+    url: '/pubs/' + pub_id + '.json',
+    dataType: 'json',
+    success: function(json) {
+      html = html + '<strong><a target="_blank" href="/pubs/' + json.pub.slug.slug.name + '">' + json.pub.name + '</a></strong><br/>';              
+      html = html + json.pub.address_1 + '<br/>';
+      if(json.pub.address_2){
+        html = html + json.pub.address_2 + '<br/>';
+      }
+      html = html + json.pub.town + '<br/>';
+      html = html + json.pub.post_code + '<br/>';
+      html = html + json.pub.telephone + '<br/>';
+    },
+    data: {},
+    async: false
+  });
+  return html;
 }
 
 
@@ -101,15 +103,15 @@ return html;
 // zoomed in area
 function update_non_clustered_markers()
 {
-		var bounds = map.getBounds();
-		var southWest = bounds.getSouthWest();
+  var bounds = map.getBounds();
+	var southWest = bounds.getSouthWest();
   var northEast = bounds.getNorthEast();	
-		$.ajax({
-   type: 'GET',
-  url: '/pubs.js',
-  dataType: 'script',
-  data: {min_lat: southWest.lat(), max_lat: northEast.lat(), 
-    min_lng: southWest.lng(), max_lng: northEast.lng()}
+	$.ajax({
+    type: 'GET',
+    url: '/pubs.js',
+    dataType: 'script',
+    data: {min_lat: southWest.lat(), max_lat: northEast.lat(), 
+           min_lng: southWest.lng(), max_lng: northEast.lng()}
   });
 }
 
@@ -121,4 +123,11 @@ function numbered_icon(number)
 	icon.iconAnchor=new GPoint(16,32);
 	icon.infoWindowAnchor=new GPoint(16, 0);	
  	return icon;	
+}
+
+function remove_non_clustered_markers()
+{  
+  for (i = 0; i < non_clustered_markers.length; i++) {
+    map.removeOverlay(non_clustered_markers[i]);
+  }
 }
