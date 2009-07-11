110d93fa21d26ce8416fecb6e921f09540972665
Matt Fawcett <mail@matthewfawcett.co.uk>
2009-07-11 21:00:58 +0100

    got the map fully functional now

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,16 +3,20 @@ $(document).ready(function(){
 });
 
 
+var icon = new GIcon(G_DEFAULT_ICON);  
+var markers = []; 
+var markerCluster;
+var clustering_on = true;
+var map;
 function initialize() {
 
   if(GBrowserIsCompatible()) {
-    var map = new GMap2(document.getElementById('map'));
+    
+    map = new GMap2(document.getElementById('map'));
     map.setCenter(new GLatLng(54, -1), 6);
     map.addControl(new GLargeMapControl());
-    var icon = new GIcon(G_DEFAULT_ICON);  
-    var markers = []; 
-    var markerCluster;
-    var clustering_on = true;
+    
+    
     
     
     $.getJSON('/pubs.json','', function(json)
@@ -47,13 +51,23 @@ function initialize() {
 		});				
     
   }
-  
-  
-  //create a marker object for the cluster
-  function createMarker(point, pub_id)
-	{
-		var marker = new GMarker(point, icon);
-		GEvent.addListener(marker, "click", function() {marker.openInfoWindowHtml(marker_info_window_html_for_pub(pub_id));});
+}
+
+
+//create a marker object for the cluster, (if a number is passed in then a numbered icon will be used)
+function createMarker(point, pub_id, number)
+{
+		if(number)
+		{
+			var icon = numbered_icon(number);
+			var marker = new GMarker(point, icon);			
+		}
+		else
+		{
+		  var marker = new GMarker(point, icon);
+		}
+		
+	  GEvent.addListener(marker, "click", function() {marker.openInfoWindowHtml(marker_info_window_html_for_pub(pub_id));});
 		return marker;
 	};
 	
@@ -63,40 +77,48 @@ function initialize() {
 	{
 		var html = '';
 		$.ajax({
-            type: 'GET',
-            url: '/pubs/' + pub_id + '.json',
-            dataType: 'json',
-            success: function(json) {
-              html = html + '<strong><a target="_blank" href="/pubs/' + json.pub.slug.slug.name + '">' + json.pub.name + '</a></strong><br/>';              
-              html = html + json.pub.address_1 + '<br/>';
-              if(json.pub.address_2){
-                html = html + json.pub.address_2 + '<br/>';
-              }
-              html = html + json.pub.town + '<br/>';
-              html = html + json.pub.post_code + '<br/>';
-              html = html + json.pub.telephone + '<br/>';
-            },
-            data: {},
-            async: false
-    });
-		return html;
-	}
-	
-	
-	// gets called when zoom level is >= 14, use ajax to get more information on pubs for this
-	// zoomed in area
-	function update_non_clustered_markers()
-	{
+   type: 'GET',
+  url: '/pubs/' + pub_id + '.json',
+  dataType: 'json',
+  success: function(json) {
+    html = html + '<strong><a target="_blank" href="/pubs/' + json.pub.slug.slug.name + '">' + json.pub.name + '</a></strong><br/>';              
+    html = html + json.pub.address_1 + '<br/>';
+    if(json.pub.address_2){
+      html = html + json.pub.address_2 + '<br/>';
+    }
+    html = html + json.pub.town + '<br/>';
+    html = html + json.pub.post_code + '<br/>';
+    html = html + json.pub.telephone + '<br/>';
+  },
+  data: {},
+  async: false
+});
+return html;
+}
+
+
+// gets called when zoom level is >= 14, use ajax to get more information on pubs for this
+// zoomed in area
+function update_non_clustered_markers()
+{
 		var bounds = map.getBounds();
 		var southWest = bounds.getSouthWest();
-	  var northEast = bounds.getNorthEast();	
+  var northEast = bounds.getNorthEast();	
 		$.ajax({
-      type: 'GET',
-      url: '/pubs.js',
-      dataType: 'script',
-      data: {min_lat: southWest.lat(), max_lat: northEast.lat(), 
-             min_lng: southWest.lng(), max_lng: northEast.lng()}
-    });
-	}
-		
+   type: 'GET',
+  url: '/pubs.js',
+  dataType: 'script',
+  data: {min_lat: southWest.lat(), max_lat: northEast.lat(), 
+    min_lng: southWest.lng(), max_lng: northEast.lng()}
+  });
+}
+
+function numbered_icon(number)
+{
+	var icon = new GIcon();	
+	icon.image="http://www.realalehunter.co.uk/img/markers/marker" + number + ".png";
+	icon.iconSize=new GSize(20,34);
+	icon.iconAnchor=new GPoint(16,32);
+	icon.infoWindowAnchor=new GPoint(16, 0);	
+ 	return icon;	
 }
