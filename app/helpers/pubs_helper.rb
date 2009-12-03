9f64057cafafe41df1fd7a76c3142a06b7f30d17
Matthew Fawcett <mattfawcett@Matthew-Fawcetts-MacBook-Pro.local>
2009-12-03 15:54:53 +0000

    unneeded changes

--- app/helpers/pubs_helper.rb
+++ app/helpers/pubs_helper.rb
@@ -1,9 +1,10 @@
 module PubsHelper
   def lightbox_image_tag(image)
     link_to(
-            image_tag(image.attachment.url(:thumb), :alt => pub_photo_alt_text(image)),
+            image_tag(image.attachment.url(:thumb), 
+            :alt => pub_photo_alt_text(image)),
             image.attachment.url(:medium),
             :rel => "lightbox",
-            :title => pub_photo_alt_text(image))      
+            :title => h(pub_photo_alt_text(image)))
   end
 end
