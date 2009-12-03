d690946c974e2d371fed4a654936426cdd0fddbb
Matthew Fawcett <mattfawcett@Matthew-Fawcetts-MacBook-Pro.local>
2009-12-03 15:54:14 +0000

    added code to add original link

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,3 +4,11 @@ function change_id_of_dynamically_added_field(html, new_id)
 	html = html.replace(/attributes\]\[(\d)+\]/g, "attributes][" + new_id + "]");
 	return html;
 }
+jQuery(function($) {
+	jQuery("a[rel^='lightbox']").slimbox({}, 
+										 function(el) {
+										   var title = el.title + " | <a href=\"" + el.href.replace("medium", "original") + "\" target=\"_blank\">Download Original</a>";
+										   return [el.href, title];
+										 }
+	);
+});
