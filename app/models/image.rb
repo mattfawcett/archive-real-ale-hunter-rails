c2c4485fb0de24683203e6cbc6e373a2d22e137a
Matthew Fawcett <mattfawcett@Matthew-Fawcetts-MacBook-Pro.local>
2010-07-01 18:14:54 +0100

    added attachment image validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,7 @@ class Image < ActiveRecord::Base
   include ActionView::Helpers::DateHelper
   
   has_attached_file :attachment, :styles => { :medium => "800x600>", :thumb => "180x150>", :big_thumb => "270x225>" }
+  validates_attachment_presence :attachment
   belongs_to :user
   belongs_to :pub
   
