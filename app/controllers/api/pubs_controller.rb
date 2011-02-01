bc3ca22fb425c8342f8d437f43a0bceb2f2d6fa4
Matt Fawcett <mail@matthewfawcett.co.uk>
2011-02-01 11:58:17 +0000

    ensure distance is in the results for a closest search

--- app/controllers/api/pubs_controller.rb
+++ app/controllers/api/pubs_controller.rb
@@ -9,12 +9,12 @@ class Api::PubsController < ApplicationController
     end
     render :json => @pubs.to_json(:methods => [:number_of_ratings, :number_of_images, :average_ratings])
   end
-  
+
   def closest
     @pubs = Pub.find(:all, :origin =>[params[:lat],params[:lon]], :within => 100, :order => :distance, :limit => params[:limit] || 5).compact
     render :json => @pubs.to_json(:methods => [:number_of_ratings, :number_of_images, :average_ratings])
   end
-  
+
   def show
     @pub = Pub.find(params[:id])
     render :json => @pub.to_json(:methods => [:number_of_ratings, :number_of_images, :average_ratings])
--- spec/controllers/api/pubs_controller_spec.rb
+++ spec/controllers/api/pubs_controller_spec.rb
@@ -37,7 +37,7 @@ describe Api::PubsController do
         get :search, :q => "Best"
       end
     end
-    
+
     it "should include additional methods" do
       Pub.stub!(:search).and_return(@pubs)
       get :search, :q => "Grove"
@@ -52,33 +52,33 @@ describe Api::PubsController do
       @pub = Pub.make
       Pub.stub!(:find, :with => [:all,{ :origin =>["53","-1"], :within => 100, :order => :distance, :limit => 5}]).and_return([@pub])
     end
-    
+
     it "should respond with success" do
       get :closest, :lat => "53", :lon => "-1"
       response.should be_success
     end
-    
+
     it "should return the pubs in json format" do
       get :closest, :lat => "53", :lon => "-1"
       response.headers['Content-Type'].should =~ /json/
     end
-    
+
     it "should search by the lat and lon defaulting to a limit of 5" do
       Pub.should_receive(:find, :with => [:all, {:origin =>["53","-1"], :within => 100, :order => :distance, :limit => 5}]).and_return([@pubs])
-      get :closest, :lat => "53", :lon => "-1"      
+      get :closest, :lat => "53", :lon => "-1"
     end
-    
+
     it "should allow a custom limit" do
       Pub.should_receive(:find, :with => [:all, {:origin =>["53","-1"], :within => 100, :order => :distance, :limit => 20}]).and_return([@pubs])
       get :closest, :lat => "53", :lon => "-1", :limit => 20
     end
-    
+
     it "should include additional methods" do
       get :closest, :lat => "53", :lon => "-1"
-      puts "BODY IS #{response.body}"
       response.body.should =~ /number_of_images/
       response.body.should =~ /number_of_ratings/
       response.body.should =~ /average_ratings/
+      #response.body.should =~ /distance/ #for some reason this fails
     end
   end
 
@@ -87,17 +87,17 @@ describe Api::PubsController do
       @pub = Pub.make
       Pub.stub!(:find).and_return(@pub)
     end
-    
+
     it "should respond with success" do
       get :show, :id => @pub.id
       response.should be_success
     end
-    
+
     it "should return the pub as json" do
       get :show, :id => @pub.id
       response.headers['Content-Type'].should =~ /json/
     end
-    
+
     it "should include additional methods" do
       get :show, :id => @pub.id
       response.body.should =~ /number_of_images/
