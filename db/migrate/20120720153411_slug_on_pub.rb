2e788d0ec39f670e287d92b7a3a25e1f6df89584
Matt Fawcett <mail@matthewfawcett.co.uk>
2012-09-03 16:12:04 +0100

    Fix the migration I think

--- db/migrate/20120720153411_slug_on_pub.rb
+++ db/migrate/20120720153411_slug_on_pub.rb
@@ -1,13 +1,16 @@
 class SlugOnPub < ActiveRecord::Migration
   def up
+    rename_table :slugs, :friendly_id_slugs
     add_column :pubs, :slug, :string
+    rename_column :friendly_id_slugs, :name, :slug
     Pub.all.each do |p|
-      p.slug = p.slugs.last
+      p.slug = p.slugs.first.slug
       p.save(:validate => false)
     end
   end
 
   def down
+    rename_table :friendly_id_slugs, :slugs
     remove_column :pubs, :slug
   end
 end
