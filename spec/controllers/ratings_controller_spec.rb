fab6b545e17362dd9808ffe0f6682275d69f7174
Matt Fawcett <mail@matthewfawcett.co.uk>
2012-09-04 20:24:29 +0100

    Respace

--- spec/controllers/ratings_controller_spec.rb
+++ spec/controllers/ratings_controller_spec.rb
@@ -5,40 +5,40 @@ describe RatingsController do
     @rating = Rating.make
     @pub = @rating.pub
     @user_who_has_rated = @rating.user
-    @user_who_has_not_rated = User.make    
+    @user_who_has_not_rated = User.make
   end
 
   describe "responding to GET new" do
     before(:each){login_as(@user_who_has_not_rated)}
-    describe "if I have not already added a review for this pub" do      
-      it "should give success" do        
+    describe "if I have not already added a review for this pub" do
+      it "should give success" do
         get :new, :pub_id => @pub.id
         response.should be_success
       end
-      
+
       it "should expose a new rating as @rating" do
-        get :new, :pub_id => @pub.id        
+        get :new, :pub_id => @pub.id
         assigns(:rating).should be_a(Rating)
         assigns(:rating).should be_new_record
       end
     end
-    
+
     describe "if I have already added a review for this page" do
       it "should redirect to the edit rating path" do
         login_as(@user_who_has_rated)
         get :new, :pub_id => @pub.id
         response.should redirect_to(edit_rating_path(@rating))
       end
-    end  
+    end
   end
-  
+
   describe "responding to GET edit" do
     before(:each){login_as(@user_who_has_rated)}
     it "should give success" do
       get :edit, :id => @rating.id
       response.should be_success
     end
-    
+
     it "should expose my rating as @rating" do
       get :edit, :id => @rating.id
       assigns(:rating).should be_a(Rating)
@@ -46,9 +46,9 @@ describe RatingsController do
     end
   end
 
-  describe "responding to POST create" do    
+  describe "responding to POST create" do
     before(:each){login_as(@user_who_has_not_rated)}
-    
+
     describe "when everything is valid" do
       it "should save my rating" do
         mock_rating = mock_model(Rating, :pub => @pub)
@@ -58,56 +58,56 @@ describe RatingsController do
         mock_rating.should_receive(:save).and_return(true)
         post :create, :pub_id => @pub.id, :rating => {'these' => 'params'}
       end
-      
+
       it "should set a flash success message" do
-        mock_rating = mock_model(Rating, :pub => @pub, :pub= => true, :user_id= => true, :save => true)        
+        mock_rating = mock_model(Rating, :pub => @pub, :pub= => true, :user_id= => true, :save => true)
         Rating.stub!(:new).with('these' => 'params').and_return(mock_rating)
         post :create, :pub_id => @pub.id, :rating => {'these' => 'params'}
         flash[:notice].should eql("Thanks, your rating has been added")
       end
-      
+
       it "should redirect me to the pubs show page" do
-        mock_rating = mock_model(Rating, :pub => @pub, :pub= => true, :user_id= => true, :save => true)        
+        mock_rating = mock_model(Rating, :pub => @pub, :pub= => true, :user_id= => true, :save => true)
         Rating.stub!(:new).with('these' => 'params').and_return(mock_rating)
         post :create, :pub_id => @pub.id, :rating => {'these' => 'params'}
         response.should redirect_to(pub_path(@pub))
       end
     end
-    
+
     describe "when validation fails" do
       it "should rerender the form if the rating is invalid" do
-        mock_rating = mock_model(Rating, :pub => @pub, :pub= => true, :user_id= => true, :save => false)        
+        mock_rating = mock_model(Rating, :pub => @pub, :pub= => true, :user_id= => true, :save => false)
         Rating.stub!(:new).with('these' => 'params').and_return(mock_rating)
         post :create, :pub_id => @pub.id, :rating => {'these' => 'params'}
         response.should be_success
       end
-      
+
       it "should expose the rating as @rating" do
-        mock_rating = mock_model(Rating, :pub => @pub, :pub= => true, :user_id= => true, :save => false)        
+        mock_rating = mock_model(Rating, :pub => @pub, :pub= => true, :user_id= => true, :save => false)
         Rating.stub!(:new).with('these' => 'params').and_return(mock_rating)
         post :create, :pub_id => @pub.id, :rating => {'these' => 'params'}
         assigns(:rating).should be_a(Rating)
       end
     end
   end
-  
+
   describe "responding to PUT update" do
     before(:each){login_as(@user_who_has_rated)}
-    
+
     describe "when everything is valid" do
       it "should save my rating" do
         @user_who_has_rated.stub_chain(:ratings, :find).and_return(@rating)
         @rating.should_receive(:update_attributes).with('these' => 'params').and_return(true)
         put :update, :id => @rating.id, :rating => {'these' => 'params'}
       end
-      
+
       it "should set a flash success message" do
         @user_who_has_rated.stub_chain(:ratings, :find).and_return(@rating)
         @rating.should_receive(:update_attributes).with('these' => 'params').and_return(true)
         put :update, :id => @rating.id, :rating => {'these' => 'params'}
         flash[:notice].should eql("Thanks, your rating has been updated")
       end
-      
+
       it "should redirect me to the pubs show page" do
         @user_who_has_rated.stub_chain(:ratings, :find).and_return(@rating)
         @rating.should_receive(:update_attributes).with('these' => 'params').and_return(true)
@@ -115,7 +115,7 @@ describe RatingsController do
         response.should redirect_to(pub_path(@pub))
       end
     end
-    
+
     describe "when validation fails" do
       it "should rerender the form if the rating is invalid" do
         @user_who_has_rated.stub_chain(:ratings, :find).and_return(@rating)
@@ -123,7 +123,7 @@ describe RatingsController do
         put :update, :id => @rating.id, :rating => {'these' => 'params'}
         response.should be_success
       end
-      
+
       it "should expose the rating as @rating" do
         @user_who_has_rated.stub_chain(:ratings, :find).and_return(@rating)
         @rating.should_receive(:update_attributes).with('these' => 'params').and_return(false)
@@ -132,5 +132,5 @@ describe RatingsController do
       end
     end
   end
-  
+
 end
