8a261a76ca7ef14a392c2c1879feac0113ff6591
Matt Fawcett <fawcett@viddler.com>
2013-08-19 11:02:52 +0100

    Change to cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,16 +7,17 @@ default_run_options[:pty] = true
 
 set :repository,  "git@bitbucket.org:mattfawcett/real-ale-hunter-rails.git"
 set :deploy_to, "/var/www/realalehunter"
-set :branch, "upgrade"
+set :branch, "digital-ocean"
 set :scm, :git
 set :user, :deploy
+set :use_sudo, false
 set :port, 1985
 
 
 
-role :web, "new.realalehunter.co.uk"                          # Your HTTP server, Apache/etc
-role :app, "new.realalehunter.co.uk"                          # This may be the same as your `Web` server
-role :db,  "new.realalehunter.co.uk", :primary => true # This is where Rails migrations will run
+role :web, "37.139.22.219"                          # Your HTTP server, Apache/etc
+role :app, "37.139.22.219"                          # This may be the same as your `Web` server
+role :db,  "37.139.22.219", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
